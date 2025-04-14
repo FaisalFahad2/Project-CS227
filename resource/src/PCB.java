302dae1e82bb428f3daa3eca7b885a1a93cf6983
Mohammd <mohmad541@gmail.com>
2025-04-14 12:03:24 +0300

    Added isloaded attrbute with setter and getter. Added WaitingCycles for each process to detect starvation, getter and increment method.

--- resource/src/PCB.java
+++ resource/src/PCB.java
@@ -6,7 +6,9 @@ public class PCB {
     private int turnaroundTime;
     private int waitingTime;
     private int priority;
-    
+    private boolean loaded = false;
+    private int waitingCycles;
+
     public PCB(int pid, int burstTime, int priority, int requiredMemory) {
         this.PID = pid;
         this.burstTime = burstTime;
@@ -15,6 +17,7 @@ public class PCB {
         this.state = "NEW";
         this.turnaroundTime = 0;
         this.waitingTime = 0;
+        this.waitingCycles=0;
     }
 
     // Getters and Setters
@@ -25,40 +28,65 @@ public class PCB {
     public String getState() {
         return state;
     }
+
     public void setState(String state) {
 
         this.state = state;
     }
+
     public int getBurstTime() {
         return burstTime;
     }
+
     public void setBurstTime(int burstTime) {
         this.burstTime = burstTime;
     }
+
     public int getRequiredMemory() {
         return requiredMemory;
     }
+
     public int getTurnaroundTime() {
         return turnaroundTime;
     }
+
     public void setTurnaroundTime(int turnaroundTime) {
         this.turnaroundTime = turnaroundTime;
     }
+
     public int getWaitingTime() {
         return waitingTime;
     }
+
     public void setWaitingTime(int waitingTime) {
         this.waitingTime = waitingTime;
     }
+
     public int getPriority() {
         return priority;
     }
+
     public void setPriority(int priority) {
         this.priority = priority;
     }
+
     public String toString() {
         return "PID: " + PID + " State: " + state + " Burst Time: " + burstTime + " Required Memory: " + requiredMemory + " Priority: " + priority;
     }
 
 
+    public boolean isLoaded() {
+        return loaded;
+    }
+
+    public void setLoaded(boolean loaded) {
+        this.loaded = loaded;
+    }
+
+    public int getWaitingCycles() {
+        return waitingCycles;
+    }
+    public void incrementWaitingCycles() {
+        this.waitingCycles++;
+    }
 }
