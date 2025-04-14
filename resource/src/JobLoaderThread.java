2a8b53ccce0a75060ab589bdb1d0874f8e12a8e7
Mohammd <mohmad541@gmail.com>
2025-04-14 12:04:20 +0300

    Added process.isLoaded to prevent loading the process again in the job queue.

--- resource/src/JobLoaderThread.java
+++ resource/src/JobLoaderThread.java
@@ -1,33 +1,33 @@
 public class JobLoaderThread implements Runnable {
 
-  private SystemCalls sysCall;
+    private SystemCalls sysCall;
 
-  public JobLoaderThread(SystemCalls systemCalls) {
-    if (systemCalls == null) {
-      throw new IllegalArgumentException("System Call cannot be null");
-    }
-    this.sysCall = systemCalls;
-  }
-
-  public void run() {
-    while (!sysCall.isJobQueueEmpty()) {
-      PCB process = sysCall.getJobQueue();
-
-      if (process == null)
-        continue;
-
-      synchronized (sysCall) {
-        while (!sysCall.allocate(process)) {
-          sysCall.print("Memory allocation failed for process " + process.getPid());
-          try {
-            sysCall.wait();
-          } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-          }
+    public JobLoaderThread(SystemCalls systemCalls) {
+        if (systemCalls == null) {
+            throw new IllegalArgumentException("System Call cannot be null");
         }
-        sysCall.addToReadyQueue(process);
-      }
-
+        this.sysCall = systemCalls;
     }
-  }
+
+    public void run() {
+        while (!sysCall.isJobQueueEmpty()) {
+            PCB process = sysCall.getJobQueue();
+
+            if (process == null|| process.isLoaded())
+                continue;
+
+            synchronized (sysCall) {
+                while (!sysCall.allocate(process)) {
+                    sysCall.print("Memory allocation failed for process " + process.getPid());
+                    try {
+                        sysCall.wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                sysCall.addToReadyQueue(process);
+            }
+
+        }
+    }
 }
