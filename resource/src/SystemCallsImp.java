3cdc57497fb4783e30942f321a34dbff615fe555
Mohammd <mohmad541@gmail.com>
2025-04-14 12:06:34 +0300

    Added isLoaded in addToReadyQueue to prevent loading a process again in the ready queue, ( this is not nesscery since i put it also in job queue add, but احتياط)

--- resource/src/SystemCallsImp.java
+++ resource/src/SystemCallsImp.java
@@ -4,29 +4,29 @@ public class SystemCallsImp implements SystemCalls {
     private jobQueue jobQueue;
     private ReadyQueue readyQueue;
     private memoryManagment memoryManager;
-    
+
     public SystemCallsImp(jobQueue jobQueue, ReadyQueue readyQueue, memoryManagment memoryManagment) {
         this.jobQueue = jobQueue;
         this.readyQueue = readyQueue;
         this.memoryManager = memoryManagment;
     }
-    
+
     public void crateProcess(int pid, int burstTime, int priority, int requiredMemory) {
         PCB process = new PCB(pid, burstTime, priority, requiredMemory);
         jobQueue.addJob(process);
     }
 
     public boolean allocate(PCB process) {
-        int requiredMemory = process.getRequiredMemory();  
+        int requiredMemory = process.getRequiredMemory();
         return memoryManager.allocateMemory(requiredMemory);
     }
-    
+
     public void exec(PCB process) {
-        
+
     }
-    
+
     public void terminated(PCB process) {
-        
+
         process.setState("TERMINATED");
         jobQueue.removeJob(process);
         readyQueue.removeProcess(process);
@@ -36,38 +36,43 @@ public class SystemCallsImp implements SystemCalls {
             this.notify();
         }
     }
-    
+
     public void deallocate(PCB process) {
         int requiredMemory = process.getRequiredMemory();
         memoryManager.deallocateMemory(requiredMemory);
     }
-    
+
     public void getpid(PCB process) {
-        
+
     }
 
     public boolean isJobQueueEmpty() {
         return jobQueue.isEmpty();
     }
-    
+
     public void print(String str) {
         System.out.println(str);
     }
-    
+
     public PCB getJobQueue() {
         return jobQueue.getJob();
     }
-    
+
     public PCB getNextReadyQueue() {
         return readyQueue.getNextProcess();
     }
+
     public boolean removeJob(PCB process) {
         return jobQueue.removeJob(process);
     }
+
     public void addToReadyQueue(PCB process) {
-        readyQueue.addProcess(process);
-        process.setState("READY");
+        if (!process.isLoaded()) {
+            readyQueue.addProcess(process);
+            process.setState("READY");
+            process.setLoaded(true);
+        }
     }
-   
+
 
 }
