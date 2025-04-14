b4b0695750864a4f3ffeea446f58def8da93cdf8
Mohammd <mohmad541@gmail.com>
2025-04-14 12:05:38 +0300

    Changed scheduler to add PriorityScheduling for testing. can be ignored.

--- resource/src/Schedular.java
+++ resource/src/Schedular.java
@@ -1,79 +1,80 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class Schedular {
-	
-	public static void main( String args[]) {
 
-		jobQueue j = new jobQueue();
-		ReadyQueue r = new ReadyQueue();
-		memoryManagment m = new memoryManagment();
-		SystemCallsImp s = new SystemCallsImp(j, r, m);
-		List<PCB> jobQueueList = j.getJobQueue();
+    public static void main(String args[]) {
 
-		FileReaderThread io = new FileReaderThread("resource//src//job.txt", s);
-		Thread t1 = new Thread(io);
-		t1.start();
+        jobQueue j = new jobQueue();
+        ReadyQueue r = new ReadyQueue();
+        memoryManagment m = new memoryManagment();
+        SystemCallsImp s = new SystemCallsImp(j, r, m);
+        List<PCB> jobQueueList = j.getJobQueue();
+        FileReaderThread io = new FileReaderThread("resource//src//job.txt", s);
+        PriorityScheduling Ps = new PriorityScheduling(s, r);
+        Thread t1 = new Thread(io);
+        t1.start();
 
-		try {
-			t1.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		j.printJobQueue();
-		
-		JobLoaderThread jobLoaderThread = new JobLoaderThread(s);
-		Thread t2 = new Thread(jobLoaderThread);
-		t2.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        j.printJobQueue();
 
-		try{
-		Thread.sleep(3000);
-		}catch(InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println("First");
-		r.printReadyQueue();
-		s.terminated(jobQueueList.get(0));
+        JobLoaderThread jobLoaderThread = new JobLoaderThread(s);
+        Thread t2 = new Thread(jobLoaderThread);
+        t2.start();
 
-		try{
-			Thread.sleep(3000);
-			}catch(InterruptedException e) {
-				e.printStackTrace();
-			}
-		System.out.println("After Termination of Process 1");
-		r.printReadyQueue();
-		
-
-		try{
-			Thread.sleep(3000);
-			}catch(InterruptedException e) {
-				e.printStackTrace();
-			}
-		
-		s.terminated(jobQueueList.get(0));
-		try{
-			Thread.sleep(3000);
-			}catch(InterruptedException e) {
-				e.printStackTrace();
-			}
-		System.out.println("After Termination of Process 2");
-		r.printReadyQueue();
-
-		try{
-			Thread.sleep(3000);
-			}catch(InterruptedException e) {
-				e.printStackTrace();
-			}
-		s.terminated(jobQueueList.get(0));
-		try{
-			Thread.sleep(3000);
-			}catch(InterruptedException e) {
-				e.printStackTrace();
-			}
-		System.out.println("After Termination of Process 3");
-		r.printReadyQueue();
-	
-
-		
-	}
-
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("First");
+        Ps.run();
+//        r.printReadyQueue();
+//        s.terminated(jobQueueList.get(0));
+//
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("After Termination of Process 1");
+//        r.printReadyQueue();
+//
+//
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        s.terminated(jobQueueList.get(0));
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("After Termination of Process 2");
+//        r.printReadyQueue();
+//
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        s.terminated(jobQueueList.get(0));
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("After Termination of Process 3");
+//        r.printReadyQueue();
+//
+//
+//    }
+    }
 }
