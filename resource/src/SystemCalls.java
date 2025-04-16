d441ec6bb7dd4b01c57909ad1bba3d75d4015c8b
Mohammd <mohmad541@gmail.com>
2025-04-16 09:01:27 +0300

    Delete unneeded system calls

--- resource/src/SystemCalls.java
+++ resource/src/SystemCalls.java
@@ -1,17 +1,12 @@
 public interface SystemCalls {
 
 
-    public void exec(PCB process);
-
     public void terminated(PCB process);
 
     public boolean allocate(PCB process);
 
     public void deallocate(PCB process);
 
-    public void getpid(PCB process);
-
-    public void print(String str);
 
     public void crateProcess(int pid, int burstTime, int priority, int requiredMemory);
 
@@ -19,9 +14,6 @@ public interface SystemCalls {
 
     public PCB getJobQueue();
 
-    public PCB getNextReadyQueue();
-
-    public boolean removeJob(PCB process);
 
     public void addToReadyQueue(PCB process);
 
