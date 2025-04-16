e4162e4d637735798abff9622d35d96bc5202683
Bilal <bilal.swe.ite@gmail.com>
2025-04-16 22:38:49 +0300

    Update jobQueue.java

--- resource/src/jobQueue.java
+++ resource/src/jobQueue.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class jobQueue {
   private List<PCB> jobQueueList;
+  private ArrayList<Integer> jobQueueListPid = new ArrayList<>();
   private int currentProcessIndex;
   private int numOfProcsess;
 
@@ -18,10 +19,11 @@ public class jobQueue {
       return;
     }
 
-    if (jobQueueList.contains(process)) {
-      System.out.println("Process already exists in the job queue");
+    if (jobQueueList.contains(process) || jobQueueListPid.contains(process.getPid())) {
+      System.out.println("Process P" + process.getPid() + " already exists in the job queue");
       return;
     }
+    jobQueueListPid.add(process.getPid());
     jobQueueList.add(process);
     numOfProcsess++;
   }
