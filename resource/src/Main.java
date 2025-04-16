203f282e38d75899fbeedfb6873c910a502e0c11
Mohammd <mohmad541@gmail.com>
2025-04-16 09:05:49 +0300

    Update Main

--- resource/src/Main.java
+++ resource/src/Main.java
@@ -108,6 +108,7 @@ public class Main {
                                 System.out.println();
                                 break;
                         }
+                        break;
                     case 5:
                         state = false;
                         break;
